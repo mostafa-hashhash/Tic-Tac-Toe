5d0784b00e403711ffe8c56c6cd84b02760d963b
Mostafa Hashhash <mostafasaeed483@gmail.com>
2019-10-01 14:00:05 +0200

    get it up

---
+++ algorithm.py
@@ -0,0 +1,15 @@
+# This Is The Algorithm
+ def MiniMax():
+     pass
+
+
+
+def f():
+    print(" f Function")
+
+def w(f):
+    def z():
+        print("Before")
+        f()
+        print("After")
+    return z    
---
+++ application.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+#from algorithm.py import MiniMAx
+
+app = Flask(__name__)
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def home():
+
+    if "board" not in session :
+        session["board"] = [[None,None,None],[None,None,None],[None,None,None]]
+        session["turn"] = "x"
+
+    return render_template("index.html",
+            board = session["board"],
+            turn = session["turn"] )
+
+
+@app.route("/play/<int:r>/<int:c>")
+def play(r,c):
+    return redirect(url_for("home"))
